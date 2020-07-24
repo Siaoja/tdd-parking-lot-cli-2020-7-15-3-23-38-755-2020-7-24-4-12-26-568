d34410df98b795acc214506f4c7effea25edd0a5
Siaoja <160692147@qq.com>
2020-07-24 15:48:36 +0800

    [Story1-parkingLot][spike lu] feature, refactor: implement ParkingLot given ticket when fetch then return car and refactor ParkingLot

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,11 +1,22 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
+    Map<CarTicket, Car> ticketCarMap;
+
+    public ParkingLot(){
+        ticketCarMap = new HashMap<>();
+    }
+
     public CarTicket park(Car car) {
-        return new CarTicket();
+        CarTicket carTicket = new CarTicket();
+        ticketCarMap.put(carTicket,car);
+        return carTicket;
     }
 
     public Car fetch(CarTicket ticket) {
-        return null;
+        return ticketCarMap.get(ticket);
     }
 }
