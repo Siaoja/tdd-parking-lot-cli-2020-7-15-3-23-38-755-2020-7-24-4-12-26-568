30f7ef27a83da5ac1422e70de314dc31403f2b07
Siaoja <160692147@qq.com>
2020-07-26 18:46:18 +0800

    [Story4][spike lu] feature: overide park method given 2 cars when park then each parkinglot has 1 car.

--- src/test/java/com/oocl/cultivation/test/SmartParkingBoy.java
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoy.java
@@ -1,12 +1,32 @@
 package com.oocl.cultivation.test;
 
-import com.oocl.cultivation.ParkingBoy;
-import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.*;
 
 import java.util.List;
 
 public class SmartParkingBoy extends ParkingBoy {
     public SmartParkingBoy(List<ParkingLot> parkingLots) {
         super(parkingLots);
+    }
+
+    @Override
+    public ParkCarInfo parkCar(Car car) {
+        ParkCarInfo parkCarInfo;
+        CarTicket carTicket = null;
+        ParkingLot bestParkingLot=null;
+        int bestCapacity = 0;
+        for (ParkingLot parkingLot : super.getParkingLots()) {
+            int parkingLotCurrentCapacity = parkingLot.getMaxCapacity() - parkingLot.getTicketCarMap().size();
+            if(parkingLotCurrentCapacity > bestCapacity){
+                bestParkingLot = parkingLot;
+                bestCapacity = parkingLotCurrentCapacity;
+            }
+        }
+        if(bestParkingLot != null){
+            carTicket = bestParkingLot.park(car);
+        }
+
+        parkCarInfo = new ParkCarInfo(carTicket, (carTicket == null ? "Not enough position." : null));
+        return parkCarInfo;
     }
 }
