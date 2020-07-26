d2e00671efa6c57c3b9958a40ad4c3ef373ea30a
Siaoja <160692147@qq.com>
2020-07-26 19:39:29 +0800

    [Story5][spike lu] feature: implement park car method given 11 cars 2 different parkinglot when park car then parkinglot1 has 1 car, parkinglot2 has 10 cars.

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -2,11 +2,33 @@ package com.oocl.cultivation;
 
 import java.util.List;
 
-public class SuperSmartParkingBoy extends ParkingBoy{
+public class SuperSmartParkingBoy extends ParkingBoy {
 
     public SuperSmartParkingBoy(List<ParkingLot> parkingLots) {
         super(parkingLots);
     }
 
+    @Override
+    public ParkCarInfo parkCar(Car car) {
+        ParkCarInfo parkCarInfo;
+        CarTicket carTicket = null;
+        ParkingLot bestParkingLot = null;
+        double bestUnavailablePositionRate = 1.0;
 
+        for (ParkingLot parkingLot : super.getParkingLots()) {
+            double parkingLotUsedCapacity = parkingLot.getTicketCarMap().size();
+            double parkingLotMaxCapacity = parkingLot.getMaxCapacity();
+            double parkingLotUnavaliablePositionRate = parkingLotUsedCapacity / parkingLotMaxCapacity;
+            if (parkingLotUnavaliablePositionRate < bestUnavailablePositionRate) {
+                bestParkingLot = parkingLot;
+                bestUnavailablePositionRate = parkingLotUnavaliablePositionRate;
+            }
+        }
+        if (bestParkingLot != null) {
+            carTicket = bestParkingLot.park(car);
+        }
+
+        parkCarInfo = new ParkCarInfo(carTicket, (carTicket == null ? "Not enough position." : null));
+        return parkCarInfo;
+    }
 }
