46ed546d7c97fdf6875d7dc0f9431153f686af69
Siaoja <160692147@qq.com>
2020-07-27 16:57:55 +0800

    [story5][spike lu] refactor: modify test method should_given_2_parkingBoy_when_arrange_manager_then_return_manager_list_size_2 to satisfied requirement.

--- src/test/java/com/oocl/cultivation/test/ParkingLotServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotServiceManagerTest.java
@@ -2,6 +2,7 @@ package com.oocl.cultivation.test;
 
 import com.oocl.cultivation.ParkingBoy;
 import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.SmartParkingBoy;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParkingLotServiceManagerTest {
+    //TODO implement arrangeManager and through the test
     @Test
     void should_given_2_parkingBoy_when_arrange_manager_then_return_manager_list_size_2() {
         //given
@@ -21,11 +23,11 @@ public class ParkingLotServiceManagerTest {
         for(int i =0; i < 3; i++)
             parkingBoybParkingLots.add(new ParkingLot());
         ParkingBoy parkingBoya = new ParkingBoy(parkingBoyaParkingLots);
-        ParkingBoy parkingBoyb = new ParkingBoy(parkingBoybParkingLots);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingBoybParkingLots);
 
         //when
         parkingLotServiceManager.arrangeManager(parkingBoya);
-        parkingLotServiceManager.arrangeManager(parkingBoyb);
+        parkingLotServiceManager.arrangeManager(smartParkingBoy);
 
         //then
         assertEquals(2,parkingLotServiceManager.getManagerListSize());
